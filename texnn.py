d84657c5b5eef139e75cf307a2ef87b7565f7e77
rrtucci <tucci@ar-tiste.com>
2023-12-31 16:53:44 -0500

    First Commit

--- texnn.py
+++ texnn.py
@@ -4,7 +4,8 @@ texnn (pronounced like "Texan") is a Python script that outputs LaTex (tex)
 strings for visualizing a Neural Net (nn) as a Bayesian Network.
 
 Although texnn is stand-alone, I wrote it specifically to aid me in writing
-a chapter on transformer architectures for my book Bayesuvius.
+a chapter on transformer architectures for my free, open source book
+Bayesuvius.
 
 texnn uses the LaTex package XYPic for drawing.
 
